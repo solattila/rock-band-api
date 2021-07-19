6556dc73bcdebf291646c8d45748716c6dc9174c
Attila Soltesz <attila.soltesz13@gmail.com>
2021-07-19 18:01:03 +0200

    solving test error

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,13 +4,13 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the users objects"""
+    """Serializer for the users object"""
 
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-        def create(self, validated_data):
-            """Create a new user with encrypted password and return it"""
-            return get_user_model().objects.create_user(**validated_data)
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -43,7 +43,10 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {'email': 'test@rockbanddev.com', 'password': 'pw', 'name': 'Test'}
+        payload = {
+            'email': 'test@rockbanddev.com',
+            'password': 'pw',
+            'name': 'Test'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
