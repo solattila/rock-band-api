d801d4d05e9db3f2d5cf6bd47e21e97f27343794
Attila Soltesz <attila.soltesz13@gmail.com>
2021-07-21 11:12:37 +0200

    Refactored the rockband viewsets

--- app/rockband/views.py
+++ app/rockband/views.py
@@ -7,47 +7,38 @@ from core.models import Tag, Member
 from rockband import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRockbandAttrViewSet(viewsets.GenericViewSet,
+                              mixins.ListModelMixin,
+                              mixins.CreateModelMixin):
+    """
+    Base viewset for user owned rockband attributes
+    """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """
+        Create a new object
+        :param serializer:
+        :return: None
+        """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRockbandAttrViewSet):
+    """Manage tags in the database"""
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """
-        Create a new tag
-        :param serializer:
-        :return: None
-        """
-        serializer.save(user=self.request.user)
-
-
-class MemberViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+class MemberViewSet(BaseRockbandAttrViewSet):
     """
     Manage members in the database
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     queryset = Member.objects.all()
     serializer_class = serializers.MemberSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """
-        Create a new member
-        :param serializer:
-        :return: None
-        """
-        serializer.save(user=self.request.user)
